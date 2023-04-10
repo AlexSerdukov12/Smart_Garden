f2fa40639eb57dbeb67a2bb3ad99b3a87be6b46c
AlexSerdukov12 <73774191+AlexSerdukov12@users.noreply.github.com>
2023-04-10 19:18:52 +0300

    Add files via upload

---
+++ V1.0_smart_Garden/main/main.ino
@@ -0,0 +1,31 @@
+/// LED + LDR
+const int LDRInput=A0; //Set Analog Input A0 for LDR.
+const int LED=2;
+
+
+
+void setup() {
+Serial.begin(9600);
+pinMode(LDRInput,INPUT);
+pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  //// LED+LDR 
+  int value=analogRead(LDRInput);//Reads the Value of LDR(light).
+  Serial.println("LDR value is :");//Prints the value of LDR to Serial Monitor.
+  Serial.println(value);
+  if(value<300)
+    {
+      digitalWrite(LED,HIGH);//The LED turns ON in Dark.
+    }
+    else
+    {
+      digitalWrite(LED,LOW);//The LED turns OFF in Light.
+    }
+  /////TEMP+HUMINTY+FAMS
+
+
+}
+
+
