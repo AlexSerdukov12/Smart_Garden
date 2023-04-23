5f76b077595afc12831934f82cb00ec9db2e3952
AlexSerdukov12 <73774191+AlexSerdukov12@users.noreply.github.com>
2023-04-23 12:45:28 +0300

    Update main.ino
    fix PH sensor

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,14 @@
 #include "KY015_Sensor.h"
 #include "FanControl.h"
 #include "WaterLevelSensor.h"
-//// ph shit
+//// ph 
+float calibration_value = 21.34-0.7 ;
+int phval = 0; 
+unsigned long int avgval; 
+int buffer_arr[10],temp;
 
+float ph_act;
+///////
 int pH_Value; 
 float Voltage;
 ////
@@ -75,10 +81,32 @@ void loop() {
   }
 
 ////////////////////////////////PH SENSOR
-  pH_Value = analogRead(A4); 
-  Voltage = pH_Value * (5.0 / 1023.0); 
-  Serial.println(Voltage); 
-  delay(500); 
+
+ for(int i=0;i<10;i++) 
+ { 
+ buffer_arr[i]=analogRead(A1);
+ delay(30);
+ }
+ for(int i=0;i<9;i++)
+ {
+ for(int j=i+1;j<10;j++)
+ {
+ if(buffer_arr[i]>buffer_arr[j])
+ {
+ temp=buffer_arr[i];
+ buffer_arr[i]=buffer_arr[j];
+ buffer_arr[j]=temp;
+ }
+ }
+ }
+ avgval=0;
+ for(int i=2;i<8;i++)
+ avgval+=buffer_arr[i];
+ float volt=(float)avgval*5.0/1024/6; 
+  ph_act = -5.5 * volt + calibration_value;
+
+ Serial.print("pH Val: ");
+ Serial.println(ph_act);
 ///////////////////////////////////////////////
   Serial.println("-----------------------------------------------------------");
 
